37264fd2c636ee770da3b249b2896da4817365e5
Doug Warren <git@dpw67.oib.com>
2018-01-11 17:23:32 -0600

    Add HelloLoggingTest with testTraceLevel.

---
+++ src/test/java/com.stackify.guide.logging.slf4j/hello/HelloLoggingTest.java
@@ -0,0 +1,17 @@
+package com.stackify.guide.logging.slf4j.hello;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloLoggingTest {
+    private static final Logger log =
+            LoggerFactory.getLogger(HelloLoggingTest.class);
+
+    @Test
+    void testTraceLevel() {
+        log.trace("ENTER: main.");
+        log.info("Welcome to logging with the SLF4J API.");
+        log.trace("EXIT: main.");
+    }
+}
