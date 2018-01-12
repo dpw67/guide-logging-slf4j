392e4790f81add5b63a50b9f294b977c8bed1ccb
Doug Warren <git@dpw67.oib.com>
2018-01-11 22:22:20 -0600

    JSON formatting - Jackson

--- src/test/java/com/stackify/guide/logging/slf4j/employee/JSONTest.java
+++ src/test/java/com/stackify/guide/logging/slf4j/employee/JSONTest.java
@@ -3,13 +3,18 @@ package com.stackify.guide.logging.slf4j.employee;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.junit.jupiter.api.Test;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 class JSONTest {
     static final Logger log = LoggerFactory.getLogger(JSONTest.class);
 
     Employee employee = new Employee("John Doe");;
+    ObjectMapper mapper = new ObjectMapper();
 
     @Test void testLogString() {
         log.debug(employee.logString());
@@ -25,6 +30,12 @@ class JSONTest {
     void testGsonPretty() {
         Gson gsonPretty = new GsonBuilder().setPrettyPrinting().create();
         log.debug("Employee (Gson pretty): \n{}", gsonPretty.toJson(employee));
+    }
+
+    @Test
+    void testJackson() throws JsonProcessingException {
+        String result = mapper.writeValueAsString(employee);
+        log.debug("Employee (Jackson): {}", result);
     }
 
 }
