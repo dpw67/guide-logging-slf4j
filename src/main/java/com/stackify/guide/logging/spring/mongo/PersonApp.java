06053a91cd9785d52981caef5ea07c7c0213d1a2
Doug Warren <git@dpw67.oib.com>
2018-01-16 22:41:01 -0600

    Add Spring Boot app for Person REST Data with MongoDB.

---
+++ src/main/java/com/stackify/guide/logging/spring/mongo/PersonApp.java
@@ -0,0 +1,12 @@
+package com.stackify.guide.logging.spring.mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApp.class, args);
+    }
+}
