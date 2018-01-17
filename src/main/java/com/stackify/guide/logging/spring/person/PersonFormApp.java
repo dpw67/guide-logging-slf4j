3f50c661d7079f2f5acb9a3ccdb2b8b50445b1d8
Doug Warren <git@dpw67.oib.com>
2018-01-16 20:58:46 -0600

    Add Spring Boot app for Person web form with validation.

---
+++ src/main/java/com/stackify/guide/logging/spring/person/PersonFormApp.java
@@ -0,0 +1,12 @@
+package com.stackify.guide.logging.spring.person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonFormApp {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(PersonFormApp.class, args);
+    }
+}
---
+++ src/main/java/com/stackify/guide/logging/spring/person/PersonWebController.java
@@ -0,0 +1,33 @@
+package com.stackify.guide.logging.spring.person;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import javax.validation.Valid;
+
+@Controller
+public class PersonWebController extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/results").setViewName("results");
+    }
+
+    @GetMapping("/")
+    public String showForm(PersonForm personForm) {
+        return "form";
+    }
+
+    @PostMapping("/")
+    public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+
+        if (bindingResult.hasErrors()) {
+            return "form";
+        }
+
+        return "redirect:/results";
+    }
+}
